4f18f8044ad5240430ca2218929d5764c48fc1e5
GALINDO ALAPONT Francisco Javier <franciscojavier.galindoalapont@soprasteria.com>
2021-03-26 11:01:53 +0100

    Set up project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,23 +1,18 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { TestBed, waitForAsync } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { IonicModule } from '@ionic/angular';
+import { render } from '@testing-library/angular';
 
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-
-  beforeEach(waitForAsync(() => {
-
-    TestBed.configureTestingModule({
-      declarations: [AppComponent],
+  it('should create the app', async () => {
+    const { fixture } = await render(AppComponent, {
+      imports: [IonicModule, RouterTestingModule],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
-    }).compileComponents();
-  }));
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    });
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
   // TODO: add more tests!
-
 });
