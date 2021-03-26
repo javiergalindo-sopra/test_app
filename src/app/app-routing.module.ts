4f56fd21d79ef9e9b32aedf67629a3c4a4cb50a0
GALINDO ALAPONT Francisco Javier <franciscojavier.galindoalapont@soprasteria.com>
2021-03-26 11:18:59 +0100

    feat: Generate login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./pages/login/login.module').then(m => m.LoginPageModule),
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [
---
+++ src/app/pages/login/login.page.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+import { render } from '@testing-library/angular';
+
+import { LoginPage } from './login.page';
+
+describe('LoginPage', () => {
+  let component: LoginPage;
+  let fixture: ComponentFixture<LoginPage>;
+
+  beforeEach(async () => {
+    const renderTools = await render(LoginPage, {
+      imports: [IonicModule.forRoot()],
+      detectChanges: true,
+    });
+
+    fixture = renderTools.fixture;
+    component = fixture.componentInstance;
+  });
+
+  it('should create the login page', () => {
+    expect(component).toBeTruthy();
+  });
+});
