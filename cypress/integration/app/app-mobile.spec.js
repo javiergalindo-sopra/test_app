757a5c7e3e53ea3f5c5aec2d4e3369d5a15a52d0
GALINDO ALAPONT Francisco Javier <franciscojavier.galindoalapont@soprasteria.com>
2021-03-29 18:24:54 +0200

    Add some e2e tests

--- cypress/integration/app/app-mobile.spec.js
+++ cypress/integration/app/app-mobile.spec.js
@@ -7,4 +7,36 @@ describe('App', () => {
     cy.visit('/');
     cy.get('[data-testid=login-page]');
   });
+
+  it('should check if the provided email is invalid', () => {
+    cy.visit('/');
+
+    cy.get('input[name=email]').type('aaa');
+    cy.get('input[name=email]').blur();
+
+    cy.contains('El email introducido es inválido');
+  });
+
+  it('should check if the provided password is invalid', () => {
+    cy.visit('/');
+
+    cy.get('input[name=password]').type('aaa');
+    cy.get('input[name=password]').blur();
+
+    cy.contains('La contraseña debe contener almenos 5 caracteres');
+  });
+
+  it('should allow to submit the login form', () => {
+    cy.visit('/');
+
+    cy.get('input[name=email]').type('tester@test.com');
+    cy.get('input[name=password]').type('123456');
+    cy.get('.login-form__submit-button').click();
+
+    cy.contains('El email introducido es inválido').should('not.exist');
+
+    cy.contains('La contraseña debe contener almenos 5 caracteres').should(
+      'not.exist'
+    );
+  });
 });
